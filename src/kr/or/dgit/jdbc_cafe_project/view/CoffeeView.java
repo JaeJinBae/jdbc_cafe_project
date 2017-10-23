06edb4bf6d37cdcf2856da7ec0c63e4a5260da8f
JaejinBae <bjj7425@naver.com>
2017-10-23 23:50:39 +0900

    수정없음

--- src/kr/or/dgit/jdbc_cafe_project/view/CoffeeView.java
+++ src/kr/or/dgit/jdbc_cafe_project/view/CoffeeView.java
@@ -13,20 +13,6 @@ public class CoffeeView extends JFrame {
 	private JPanel contentPane;
 
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					CoffeeView frame = new CoffeeView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-
 	public CoffeeView() {
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
